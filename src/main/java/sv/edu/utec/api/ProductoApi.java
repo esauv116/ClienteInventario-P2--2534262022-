eed6e53fa98279854314f53c38b4ddbeb9b39150
abner esau vasquez diaz <25342620222@mail.utec.edu.sv>
2026-09-20 03:35:16 -0600

    feat: DTO para el JSON del proveedor

---
+++ src/main/java/sv/edu/utec/api/ProductoApi.java
@@ -0,0 +1,46 @@
+package sv.edu.utec.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import sv.edu.utec.modelo.Producto;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductoApi {
+    private int id;
+    private String title;
+    private int stock;
+
+    public ProductoApi() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Producto aProducto() {
+        String nombreFinal = this.title;
+        if (nombreFinal != null && nombreFinal.length() > 50) {
+            nombreFinal = nombreFinal.substring(0, 50);
+        }
+        return new Producto(this.id, nombreFinal, this.stock);
+    }
+}
